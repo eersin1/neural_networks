96376ebd747b915fb871a1b8c9b60108ac812c6a
Emre Ersin <emreersin.e@gmail.com>
2026-08-18 15:33:52 -0400

    week 1 task 3: squared error loss function

---
+++ 03_loss.py
@@ -0,0 +1,40 @@
+import math
+
+def sigmoid(n):
+    return 1/(1+math.exp(-n))
+
+def weighted(inputs, weights):
+    total = 0
+    for i in range(len(inputs)):
+        total += inputs[i]*weights[i]
+    return total
+
+def neuron(inputs, weights, bias):
+    return sigmoid(weighted(inputs,weights)+bias)
+
+def loss(actuals):
+    total = 0
+    targets = [
+        0.5,
+        0,
+        0,
+        0
+    ]
+    for i in range(len(targets)):
+        total += (actuals[i] - targets[i])**2
+    return total/len(actuals)
+    
+
+def main():
+    neurons = [
+        {"b": -2, "w": [-3.81, 1.89, 6.23, 4.56]},
+        {"b": 3, "w": [-5.39, 0.25, 2.34, 9.85]},
+        {"b": -1, "w": [3.01, 4.82, 1.23, 4.23]},
+        {"b": -10, "w": [4.32, 1.87, 6.46, 7.88]},
+    ]
+    x = [0.84, 0.43, 0.02, 0.65]
+    actuals = [neuron(x, n["w"], n["b"]) for n in neurons]
+    print(loss(actuals))
+    
+
+main()
